965b6242da006558ff78d0d050809e76c75595e5
2239504-max <2239504@donga.ac.kr>
2026-03-16 20:41:22 +0900

    main.py 업데이트

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import domestic_stocks, overseas_stocks, domestic_derivatives, overseas_derivatives
+from app.routers import portfolio
 
 app = FastAPI(title="KIS Multi-Asset API")
 
@@ -11,3 +12,4 @@ app.include_router(domestic_stocks.router)
 app.include_router(overseas_stocks.router)
 app.include_router(domestic_derivatives.router)
 app.include_router(overseas_derivatives.router)
+app.include_router(portfolio.router)
