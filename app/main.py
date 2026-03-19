53817b36766b408826077c541bc54ab7bf263c9f
2239504-max <2239504@donga.ac.kr>
2026-03-19 09:48:17 +0900

    main.py 업데이트

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routers import domestic_stocks, overseas_stocks, domestic_derivatives, overseas_derivatives
 from app.routers import portfolio, market
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="StockiTrade API",
